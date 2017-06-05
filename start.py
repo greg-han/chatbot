45674b3d2e7e84223bc73698813ebbf7c0eeb50d
ghan3 <ghan3@mail.ccsf.edu>
2017-06-04 19:37:53 -0700

    null

--- start.py
+++ start.py
@@ -2,10 +2,10 @@ from flask import Flask
 from flask import (abort,render_template,request,redirect,url_for,flash,current_app,jsonify)
 app = Flask(__name__)
 
-@app.route('/')
-@app.route('/index')
-def index():
-    return render_template('index.html') 
+@app.route('/<string:page_name>/')
+
+def index(page_name):
+    return render_template('%s.html' % page_name) 
 
 if __name__ == '__main__':
     app.run()
